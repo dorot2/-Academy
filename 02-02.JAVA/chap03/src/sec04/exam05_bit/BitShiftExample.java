0db542900d65c70b3b0ec849bd45a067000cf421
Dorothy_YANG <105154847+dorot2@users.noreply.github.com>
2022-09-06 22:31:18 +0900

    Add files via upload

---
+++ 02-02.JAVA/chap03/src/sec04/exam05_bit/BitShiftExample.java
@@ -0,0 +1,17 @@
+package sec04.exam05_bit;
+
+public class BitShiftExample {
+
+	public static void main(String[] args) {
+		
+		// 비트 이동 연산자.
+		
+		
+		// 1 << 3
+		// 1의 값을 8비트로 표현.    ex> 0000 0001
+		// 왼쪽으로 3비트 이동.      ex> 0000 1000
+		System.out.println("1 << 3 = " + (1<<3)); // 8
+		
+		
+	}
+}
